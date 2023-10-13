a6fa521002f399f6b3c508c0088a5e8606d6cda0
abdomarrok <marrokmar@gmail.com>
2023-10-13 14:32:42 -0700

    fix

--- shell.c
+++ shell.c
@@ -3,13 +3,12 @@
 /**
  * shell - A simple shell program
  *
- * @argc: The number of command-line arguments
  * @argv: An array of command-line arguments
  *
  * Return: 0 on success, or a non-zero value on failure
  */
 
-int main(int argc, char **argv)
+int main( char **argv)
 {
     char *prmt = "cisfun$ ", *buffer, *copy_buf, *token;
     size_t size = 0;
