9243e4fb202effcb32c9a34859a0900aced79935
abdomarrok <marrokmar@gmail.com>
2023-10-13 13:35:36 -0700

    tt

--- shell.c
+++ shell.c
@@ -3,6 +3,7 @@
 /**
  * shell - A simple shell program
  *
+ * @argc: The number of command-line arguments
  * @argv: An array of command-line arguments
  *
  * Return: 0 on success, or a non-zero value on failure
@@ -10,13 +11,13 @@
 
 int main(int argc, char **argv)
 {
-    char *prmt = "cisfun$ ",*buffer;
-    size_t size = 0;
+  char *prmt = "cisfun$ ", *buffer;
+  size_t size = 0;
 
-    printf("%s",prmt);
-    getline(&buffer,&size,stdin);
+  printf("%s", prmt);
+  getline(&buffer, &size, stdin);
 
-    printf("%s\n",buffer);
-    free(buffer);
-    return (0);
+  printf("%s\n", buffer);
+  free(buffer);
+  return (0);
 }
