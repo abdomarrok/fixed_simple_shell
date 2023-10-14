a05402a8f6140fc99579d6624e1952406f7a1f95
abdomarrok <marrokmar@gmail.com>
2023-10-14 11:28:34 -0700

    fix

--- shell.c
+++ shell.c
@@ -1,50 +1,56 @@
 #include "shell.h"
 
-int main(int argc,char **env)
+int main(int argc, char **env)
 {
     (void)argc;
     char *prmt = "($) ", *buffer = NULL;
-    char *argument[]={NULL,NULL};
+    char *argument[] = {NULL, NULL};
     size_t bsize;
     ssize_t num_of_chars;
     pid_t child_id;
-    int status,i;
-
+    int status, i;
 
     while (1)
     {
         _printstring(prmt);
-       num_of_chars= getline(&buffer, &bsize, stdin);
-       if(num_of_chars==-1){
-        _printstring("getting out of shell");
+        num_of_chars = getline(&buffer, &bsize, stdin);
+        if (num_of_chars == -1)
+        {
+            _printstring("getting out of shell");
             free(buffer);
-        return (-1);
-       }
-       i=0;
-while (buffer[i])
-{
-    if(buffer[i]=='\n')
-    {
-        buffer[i]=0;
-    }
-}
+           exit(0);
+        }
+        i = 0;
+        while (buffer[i])
+        {
+            if (buffer[i] == '\n')
+            {
+                buffer[i] = 0;
+                i++;
+            }
+        }
 
-
-       argument[0]= strdup(buffer);
-
-       child_id= fork();
-        if(child_id <0){
+        argument[0] = strdup(buffer);
+        child_id = fork();
+        
+        if (child_id < 0)
+        {
             perror("fork error");
             free(buffer);
-            return(-1);
-        }else if (child_id==0){
-            if(execve(argument[0],argument,env)==-1){
+            exit(0);
+        }
+        else if (child_id == 0)
+        {
+            if (execve(argument[0], argument, env) == -1)
+            {
                 _printstring("command do not exist \n ");
             };
-        }else{
+        }
+        else
+        {
             wait(&status);
         }
-      /* _printstring(buffer);*/
+        /* _printstring(buffer);*/
     }
 
     free(buffer);
--- shell.h
+++ shell.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <waitpkgintrin.h>
+#include<sys/wait.h>
 #include <string.h>
 
 
