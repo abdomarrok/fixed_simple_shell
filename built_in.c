ff313dd8265ec8079e96e81456c40b8c187c2d26
abdomarrok <marrokmar@gmail.com>
2023-10-14 14:33:27 -0700

    fix error

--- built_in.c
+++ built_in.c
@@ -1,7 +1,9 @@
 #include "shell.h"
 
-int _built_in(char **argument){
-    if(strcmp(argument[0],"exit")==0){
+int _built_in(char **argument)
+{
+    if (strcmp(argument[0],"exit")==0)
+    {
         _printstring("exit the shell");
         exit(0);
     }
