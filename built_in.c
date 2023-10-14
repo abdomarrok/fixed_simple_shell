fbc56ed1072e3197cdf20a0e6af72cfef27b6adf
abdomarrok <marrokmar@gmail.com>
2023-10-14 15:26:01 -0700

    improve

--- built_in.c
+++ built_in.c
@@ -1,7 +1,28 @@
 #include "shell.h"
 
+
+int _built_in(char **argument) {
+  switch (argument[0]) {
+    case "exit":
+      _printstring("exit the shell \n ");
+      exit(0);
+    case "cd":
+      if (argument[1] == NULL) {
+        chdir(getenv("HOME"));
+      } else {
+        chdir(argument[1]);
+      }
+      break;
+    default:
+      return 1;
+  }
+  return 0;
+}
+
+/**
 int _built_in(char **argument)
 {
+
     if (strcmp(argument[0],"exit")==0)
     {
         _printstring("exit the shell \n ");
@@ -15,4 +36,4 @@ int _built_in(char **argument)
         }
     }
     return(0);
-}
+}*/
