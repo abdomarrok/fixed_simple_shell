33c962c85e8e655ed472ff1670d3a97223d8c54e
abdomarrok <marrokmar@gmail.com>
2023-10-14 11:17:15 -0700

    shell start

--- shell.c
+++ shell.c
@@ -1,10 +1,15 @@
 #include "shell.h"
 
-int main(void)
+int main(int argc,char **env)
 {
+    (void)argc;
     char *prmt = "($) ", *buffer = NULL;
+    char *argument[]={NULL,NULL};
     size_t bsize;
     ssize_t num_of_chars;
+    pid_t child_id;
+    int status,i;
+
 
     while (1)
     {
@@ -12,9 +17,34 @@ int main(void)
        num_of_chars= getline(&buffer, &bsize, stdin);
        if(num_of_chars==-1){
         _printstring("getting out of shell");
+            free(buffer);
         return (-1);
        }
-        _printstring(buffer);
+       i=0;
+while (buffer[i])
+{
+    if(buffer[i]=='\n')
+    {
+        buffer[i]=0;
+    }
+}
+
+
+       argument[0]= strdup(buffer);
+
+       child_id= fork();
+        if(child_id <0){
+            perror("fork error");
+            free(buffer);
+            return(-1);
+        }else if (child_id==0){
+            if(execve(argument[0],argument,env)==-1){
+                _printstring("command do not exist \n ");
+            };
+        }else{
+            wait(&status);
+        }
+      /* _printstring(buffer);*/
     }
 
     free(buffer);
--- shell.h
+++ shell.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>>
+
 
 int _putchar(char c);
 int _printstring(char *s);
