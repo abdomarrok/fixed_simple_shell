ba402b51923c695444eccd3540fefa57ec3c108c
abdomarrok <marrokmar@gmail.com>
2023-10-18 06:14:55 -0700

    betty

--- handle_command.c
+++ handle_command.c
@@ -5,21 +5,25 @@
  * @buffer: The input command buffer
  * @argument: An array of arguments for the command
  */
-void handle_command(char *buffer, char **argument) 
+void handle_command(char *buffer, char **argument)
 {
-    char *path;
-    remove_newline(buffer);
-    tokenize_arguments(buffer, argument);
-    path = get_location(argument[0]);
+	char *path;
+	remove_newline(buffer);
+	tokenize_arguments(buffer, argument);
+	path = get_location(argument[0]);
 
-    if (path == NULL) {
-        if (_built_in(argument) != 0) {
-            return;
-        } else {
-            perror("command not found\n");
-            return;
-        }
-    }
+	if (path == NULL)
+	{
+		if (_built_in(argument) != 0)
+		{
+			return;
+		}
+		else
+		{
+			perror("command not found\n");
+			return;
+		}
+	}
 
-    handle_child_process(path, argument);
+	handle_child_process(path, argument);
 }
