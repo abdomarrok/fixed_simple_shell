4b7c9af0140d13867dd030626e10c35b9b75ef7d
abdomarrok <marrokmar@gmail.com>
2023-10-18 05:20:02 -0700

    betty

--- get_location.c
+++ get_location.c
@@ -9,28 +9,28 @@
  */
 char *place(char *path, char *argument)
 {
-    char *path2, *path_token, *file_path, *delim = ":";
-    path2 = strdup(path);
-    path_token = strtok(path2, delim);
+	char *path2, *path_token, *file_path, *delim = ":";
+	path2 = strdup(path);
+	path_token = strtok(path2, delim);
 
-    file_path = malloc(strlen(argument) + strlen(path_token) + 4);
+	file_path = malloc(strlen(argument) + strlen(path_token) + 4);
 
-    while (path_token != NULL)
-    {
-        strcpy(file_path, path_token);
-        strcat(file_path, "/");
-        strcat(file_path, argument);
-        strcat(file_path, "\0");
-        if (access(file_path, X_OK) == 0)
-        {
-            return (file_path);
-        }
-        path_token = strtok(NULL, delim);
-    }
-    free(file_path);
-    free(path2);
+	while (path_token != NULL)
+	{
+		strcpy(file_path, path_token);
+		strcat(file_path, "/");
+		strcat(file_path, argument);
+		strcat(file_path, "\0");
+		if (access(file_path, X_OK) == 0)
+		{
+			return (file_path);
+		}
+		path_token = strtok(NULL, delim);
+	}
+	free(file_path);
+	free(path2);
 
-    return (NULL);
+	return (NULL);
 }
 
 /**
@@ -41,14 +41,14 @@ char *place(char *path, char *argument)
  */
 char *get_location(char *command)
 {
-    char *path;
+	char *path;
 
-    path = getenv("PATH");
+	path = getenv("PATH");
 
-    if (path)
-    {
-        path = place(path, command);
-        return (path);
-    }
-    return (NULL);
+	if (path)
+	{
+		path = place(path, command);
+		return (path);
+	}
+	return (NULL);
 }
