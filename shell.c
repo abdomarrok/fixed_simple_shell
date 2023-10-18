27fb18f0050d238baf07be573f4bc335a8166151
abdomarrok <marrokmar@gmail.com>
2023-10-18 05:49:07 -0700

    betty !!!!!

--- shell.c
+++ shell.c
@@ -11,13 +11,14 @@
  *
  * Return: Always returns 0.
  */
-
 int main(int argc, char **env)
 {
-(void)argc;
-  while (1)
-  {
-    handle_shell_prompt();
-  }
-  return (0);
+    (void)argc;
+    
+    while (1)
+    {
+        handle_shell_prompt();
+    }
+
+    return (0);
 }
