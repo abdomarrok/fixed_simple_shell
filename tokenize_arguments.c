a97b9fa34399c9ecf898e82de7a0c07b3ae1189c
abdomarrok <marrokmar@gmail.com>
2023-10-18 08:59:32 -0700

    ioou_

--- tokenize_arguments.c
+++ tokenize_arguments.c
@@ -1,19 +1,51 @@
 #include "shell.h"
 
 /**
+ * is_delimiter - Check if a character is a delimiter
+ * @c: The character to check
+ * @delim: The list of delimiters
+ *
+ * Return: 1 if the character is a delimiter, 0 otherwise
+ */
+int is_delimiter(char c, const char *delim) {
+    while (*delim) {
+        if (c == *delim)
+            return 1;
+        delim++;
+    }
+    return 0;
+}
+
+/**
  * tokenize_arguments - Function to tokenize command arguments
  * @buffer: The input command buffer
  * @argument: An array of arguments for the command
  */
-void tokenize_arguments(char *buffer, char **argument)
-{
-int j = 0;
-char *delim = " \n";
+void tokenize_arguments(char *buffer, char **argument) {
+    int j = 0;
+    int in_word = 0; 
+    char *arg_start = buffer; 
 
-	argument[j] = strtok(buffer, delim);
-	while (argument[j])
-	{
-		argument[++j] = strtok(NULL, delim);
-	}
-	argument[j] = NULL;
+    while (*buffer) {
+        if (!is_delimiter(*buffer, " \n")) {
+            if (!in_word) {
+                arg_start = buffer;  
+                in_word = 1;
+            }
+        } else {
+            if (in_word) {
+                *buffer = '\0';  
+                argument[j++] = arg_start;
+                in_word = 0;
+            }
+        }
+        buffer++;
+    }
+
+    if (in_word) {
+        argument[j] = arg_start; 
+        j++;
+    }
+
+    argument[j] = NULL; 
 }
