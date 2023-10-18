533c9763eb95c2f621cc0b99f3c1cf6b1ad54aec
abdomarrok <marrokmar@gmail.com>
2023-10-18 06:56:31 -0700

    betty

--- handle_child_process.c
+++ handle_child_process.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * handle_child_process - Function to handle child processes and execute commands
+ * handle_child_process - Function to handle child processes and execute cmd
  * @path: The path to the command to execute
  * @argument: An array of arguments for the command
  * @buffer: The input buffer
@@ -10,7 +10,7 @@
 
 void handle_child_process(char *path, char **argument, char *buffer,
  char **env)
-{
+ {
     pid_t child_id;
     int status;
 
@@ -18,9 +18,9 @@ void handle_child_process(char *path, char **argument, char *buffer,
 
     if (child_id < 0)
     {
-        perror("fork error");
-        if (buffer != NULL)
-            free(buffer);
+    perror("fork error");
+    if (buffer != NULL)
+        free(buffer);
         exit(0);
     }
     else if (child_id == 0)
