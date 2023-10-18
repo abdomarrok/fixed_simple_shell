1f573dc3a2f09a0798b7b33bbaa1b1c668e898d5
abdomarrok <marrokmar@gmail.com>
2023-10-18 06:26:40 -0700

    betty

--- remove_newline.c
+++ remove_newline.c
@@ -4,12 +4,15 @@
  * remove_newline - Function to remove trailing newline character
  * @str: The string to process
  */
-void remove_newline(char *str) {
-    int i = 0;
-    while (str[i]) {
-        if (str[i] == '\n') {
-            str[i] = 0;
-        }
-        i++;
-    }
+void remove_newline(char *str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+		{
+			str[i] = 0;
+		}
+		i++;
+	}
 }
