d1e1aea65b4821d799dcefa45235578a3327ff63
abdomarrok <marrokmar@gmail.com>
2023-10-18 06:25:01 -0700

    tt

--- handle_shell_prompt.c
+++ handle_shell_prompt.c
@@ -5,22 +5,22 @@
  */
 void handle_shell_prompt(void)
 {
-    char *prmt = "($) ";
-    char *buffer = NULL;
-    size_t bsize;
-    ssize_t num_of_chars;
-    char *argument[11];
+	char *prmt = "($) ";
+	char *buffer = NULL;
+	size_t bsize;
+	ssize_t num_of_chars;
+	char *argument[11];
 
-    if (isatty(0))
-        _printstring(prmt);
+	if (isatty(0))
+		_printstring(prmt);
 
-    num_of_chars = getline(&buffer, &bsize, stdin);
+	num_of_chars = getline(&buffer, &bsize, stdin);
 
-    if (num_of_chars == -1)
-    {
-        free(buffer);
-        exit(0);
-    }
+	if (num_of_chars == -1)
+	{
+		free(buffer);
+		exit(0);
+	}
 
-    handle_command(buffer, argument);
+	handle_command(buffer, argument);
 }
