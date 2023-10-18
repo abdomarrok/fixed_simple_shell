4adb80b533d999b778ef86f294a6f9be658577a2
abdomarrok <marrokmar@gmail.com>
2023-10-18 05:47:34 -0700

    tt

--- shell.c
+++ shell.c
@@ -14,11 +14,10 @@
 
 int main(int argc, char **env)
 {
-(void)argc; 
-    while (1)
-    {
-        handle_shell_prompt();
-    }
-
-    return (0);
+(void)argc;
+  while (1)
+  {
+    handle_shell_prompt();
+  }
+  return (0);
 }
