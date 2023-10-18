70f7fe13eb1f4c0d03b44169a2f5fe6f09aac77c
abdomarrok <marrokmar@gmail.com>
2023-10-18 05:57:12 -0700

    tt

--- handle_child_process.c
+++ handle_child_process.c
@@ -1,8 +1,7 @@
 #include "shell.h"
 
 /**
- * handle_child_process - Function to handle child processes 
- * and execute commands
+ * handle_child_process  Function handle child processes & execute commands
  * @path: The path to the command to execute
  * @argument: An array of arguments for the command
  */
