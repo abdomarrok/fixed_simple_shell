1a8e01460978034ec83607244208e56f25d00202
abdomarrok <marrokmar@gmail.com>
2023-10-18 08:03:32 -0700

    mm

--- built_in.c
+++ built_in.c
@@ -3,13 +3,11 @@
 /**
  * _built_in - Check for built-in shell commands.
  * @argument: An array of command arguments.
- * @env: The array of environment variables.
  *
  * Return: 1 if a built-in command is found, 0 otherwise.
  */
-int _built_in(char **argument, char **env)
+int _built_in(char **argument)
 {
-int i = 0;
 	if (strcmp(argument[0], "exit") == 0)
 	{
 		_printstring("exit the shell\n");
@@ -24,16 +22,6 @@ int i = 0;
 		else
 		{
 			chdir(argument[1]);
-		}
-		return (1);
-	}
-	else if (strcmp(argument[0], "env") == 0)
-	{
-		while (env[i] != NULL)
-		{
-			_printstring(env[i]);
-			_printstring("\n");
-			i++;
 		}
 		return (1);
 	}
