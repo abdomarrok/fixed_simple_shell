a6fecd86caeb0ac9996fe7f27f8022ee7eb3a598
abdomarrok <marrokmar@gmail.com>
2023-10-18 08:28:42 -0700

    jhdsihs

--- built_in.c
+++ built_in.c
@@ -10,22 +10,22 @@
 int _built_in(char **argument, char **env)
 {
 if (strcmp(argument[0], "exit") == 0)
-    {
-        _printstring("exit the shell\n");
-        exit(0);
-    }
+{
+    _printstring("exit the shell\n");
+    exit(0);
+}
 else if (strcmp(argument[0], "cd") == 0)
-    {
-        if (argument[1] == NULL)
-        	{
-            		chdir(getenv("HOME"));
-        	}
-        else
-        	{
-            		chdir(argument[1]);
-        	}
+{
+    if (argument[1] == NULL)
+        {
+            chdir(getenv("HOME"));
+        }
+    else
+        {
+            chdir(argument[1]);
+        }
         return (1);
     }
-    return (0);
+return (0);
 }
 
