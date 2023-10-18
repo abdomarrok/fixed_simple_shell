c3b401d791aa92050e5f26e03d1a1d86f65202c8
abdomarrok <marrokmar@gmail.com>
2023-10-18 06:11:40 -0700

    tty

--- handle_command.c
+++ handle_command.c
@@ -1,26 +1,12 @@
 #include "shell.h"
 
 /**
- * tokenize_arguments - Function to tokenize command arguments
- * @buffer: The input command buffer
- * @argument: An array of arguments for the command
- */
-void tokenize_arguments(char *buffer, char **argument) {
-    int j = 0;
-    char *delim = " \n";
-    argument[j] = strtok(buffer, delim);
-    while (argument[j]) {
-        argument[++j] = strtok(NULL, delim);
-    }
-    argument[j] = NULL;
-}
-
-/**
  * handle_command - Function to handle command execution
  * @buffer: The input command buffer
  * @argument: An array of arguments for the command
  */
-void handle_command(char *buffer, char **argument) {
+void handle_command(char *buffer, char **argument) 
+{
     char *path;
     remove_newline(buffer);
     tokenize_arguments(buffer, argument);
---
+++ tokenize_arguments.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * tokenize_arguments - Function to tokenize command arguments
+ * @buffer: The input command buffer
+ * @argument: An array of arguments for the command
+ */
+void tokenize_arguments(char *buffer, char **argument) 
+{
+    int j = 0;
+    char *delim = " \n";
+    argument[j] = strtok(buffer, delim);
+    while (argument[j]) {
+        argument[++j] = strtok(NULL, delim);
+    }
+    argument[j] = NULL;
+}
