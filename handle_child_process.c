9470ebf1c4f887ed61014593c4733a6ff9e2aef3
abdomarrok <marrokmar@gmail.com>
2023-10-18 05:56:02 -0700

    tt

--- handle_child_process.c
+++ handle_child_process.c
@@ -7,26 +7,26 @@
  */
 void handle_child_process(char *path, char **argument)
 {
-    pid_t child_id;
-    int status;
+	pid_t child_id;
+	int status;
 
-    child_id = fork();
+	child_id = fork();
 
-    if (child_id < 0)
-    {
-        perror("fork error");
-        free(buffer);
-        exit(0);
-    }
-    else if (child_id == 0)
-    {
-        if (execve(path, argument, env) == -1)
-        {
-            perror("command does not exist\n");
-        }
-    }
-    else
-    {
-        wait(&status);
-    }
+	if (child_id < 0)
+	{
+		perror("fork error");
+		free(buffer);
+		exit(0);
+	}
+	else if (child_id == 0)
+	{
+		if (execve(path, argument, env) == -1)
+		{
+			perror("command does not exist\n");
+		}
+	}
+	else
+	{
+		wait(&status);
+	}
 }
