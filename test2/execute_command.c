d70f87594206644ebc5f74cd2ea1aca68c48f687
abdomarrok <marrokmar@gmail.com>
2023-10-19 06:00:18 -0700

    betty

--- test2/execute_command.c
+++ test2/execute_command.c
@@ -45,36 +45,3 @@ int execute_command(char **args)
 		}
 	}
 }
-
-/**
- * execute_external_command - Execute an external command.
- * @args: Command to be executed.
- */
-int execute_external_command(char **args)
-{
-	pid_t child = fork();
-
-	if (child == -1)
-	{
-		perror("problem");
-		return -1;
-	}
-	else if (child == 0)
-	{
-		execve(args[0], args, environ);
-		perror("execve");
-		_exit(EXIT_FAILURE);
-	}
-	else
-	{
-		int status;
-		int exit_status;
-
-		waitpid(child, &status, 0);
-		if ((status & 0xff) == 0)
-			exit_status = (status >> 8) & 0xff;
-		else
-			exit_status = -1;
-		return exit_status;
-	}
-}
