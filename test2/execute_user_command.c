70a5413bc6106046cad0544ebb4942d4c71ba00b
abdomarrok <marrokmar@gmail.com>
2023-10-19 06:31:13 -0700

    minimize lines

---
+++ test2/execute_user_command.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+
+/**
+ * Execute a user command based on the first argument.
+ * @args: Array of command and arguments.
+ */
+void execute_user_command(char **args)
+{
+    if (_strchr(args[0], '/') != NULL)
+    {
+        run_command(args);
+    }
+    else
+    {
+        if (_strcmp(args[0], "exit") == 0)
+        {
+            exit_shell(args);
+        }
+        else if (_strcmp(args[0], "env") == 0)
+        {
+            env_builtin();
+        }
+        else if (_strcmp(args[0], "setenv") == 0)
+        {
+            update_environment_variable(args);
+        }
+        else if (_strcmp(args[0], "unsetenv") == 0)
+        {
+            remove_environment_variable(args);
+        }
+        else if (_strcmp(args[0], "cd") == 0)
+        {
+            cd_handler(args);
+        }
+        else
+        {
+            execute_command(args);
+        }
+    }
+}
--- test2/shell.c
+++ test2/shell.c
@@ -1,64 +1,39 @@
 #include "shell.h"
 
-
 int main(void)
 {
-	while (1)
-	{
-		char **args = get_input();
-		int i;
+    while (1)
+    {
+        char **args = get_input();
 
-		if (args == NULL)
-		{
-			write(STDOUT_FILENO, "Error occurred while getting input.\n", 36);
-			continue;
-		}
+        if (args == NULL)
+        {
+            write(STDOUT_FILENO, "Error occurred while getting input.\n", 36);
+            continue;
+        }
 
-		if (args[0] == NULL)
-		{
-			for (i = 0; args[i] != NULL; i++)
-				free(args[i]);
-			free(args);
-			continue;
-		}
+        if (args[0] == NULL)
+        {
+            free_arguments(args);
+            continue;
+        }
 
-		if (_strchr(args[0], '/') != NULL)
-		{
-			run_command(args);
-		}
-		else
-		{
-			if (_strcmp(args[0], "exit") == 0)
-			{
-				exit_shell(args);
-			}
-			else if (_strcmp(args[0], "env") == 0)
-			{
-				/* Call the env_builtin function */
-				env_builtin();
-			}
-			else if (_strcmp(args[0], "setenv") == 0)
-			{
-				 updateEnvironmentVariable(args);
-			}
-			else if (_strcmp(args[0], "unsetenv") == 0)
-			{
-				removeEnvironmentVariable(args);
-			}
-			else if (_strcmp(args[0], "cd") == 0)
-			{
-				cd_handler(args);
-			}
-			else
-			{
-				execute_command(args);
-			}
-
-			for (i = 0; args[i] != NULL; i++)
-			{
-				free(args[i]);
-			}
-			free(args);
-		}
-	}
+        execute_user_command(args);
+        free_arguments(args);
+    }
 }
+
+/**
+ * Free memory allocated for an array of arguments.
+ * @args: Array of command and arguments.
+ */
+void free_arguments(char **args)
+{
+    int i;
+    for (i = 0; args[i] != NULL; i++)
+    {
+        free(args[i]);
+    }
+    free(args);
+}
+
